1346084d1b75ccd4bf7f55bdeaff7c3f8aa76a82
Shahroz <shahroz95@hotmail.com>
2023-09-27 16:40:38 +0500

    Added jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -49,26 +49,29 @@ editor = new EditorJS({
         embed: Embed
     }
 });
+$('#downloadBtn').addClass('disabled');
+$('#analyzeBtn').addClass('disabled');
+$('#contentContainer').hide();
+$('#analysisResults').hide();
 
-document.getElementById("downloadBtn").classList.add("disabled"); // Download button disabled during initialization
-document.getElementById("analyzeBtn").classList.add("disabled"); // Analyze button disabled during initialization
 /**
  * Plots chart with chart.js using the data provided
  */
 function plotChart() {
-    let selectedChartType = document.getElementById("chartType").value;
+    console.log();
+    let selectedChartType = $('#chartType').val();
 
     if (theChart) {
         theChart.destroy(); // Destroy the existing chart if it exists
     }
-
-    let hotData = hot.getData();
+    
     const ctx = document.getElementById("theChart");
     const chartData = {
         labels: [],
         datasets: [],
     };
 
+    let hotData = hot.getData();
     if (hotData.length < 2) {
         alert("Please add at least two rows of data.");
         return;
@@ -87,13 +90,13 @@ function plotChart() {
         chartData.datasets.push(dataset);
     }
 
-    const chartTheme = document.getElementById("chartTheme").value;
-    const chartBackground = document.getElementById("chartBackground").value;
-    const chartTitle = document.getElementById("chartTitle").value;
-    const chartSubtitle = document.getElementById("chartSubtitle").value;
-    const legendPosition = document.getElementById("legendPosition").value;
+    const chartTheme = $('#chartTheme').val();
+    const chartBackground = $('#chartBackground').val();
+    const chartTitle = $('#chartTitle').val();
+    const chartSubtitle = $('#chartSubtitle').val();
+    const legendPosition = $('#legendPosition').val();
 
-    const chartHeight = document.getElementById("chartHeight").value;
+    const chartHeight = $('#chartHeight').val(); 
     ctx.style.height = chartHeight + "px";
 
     theChart = new Chart(ctx, {
@@ -176,17 +179,19 @@ function plotChart() {
         ctx.style.backgroundColor = "white";
     }
 
+    $('#analysisResults').removeClass();
     if (chartBackground == 'bg-light') {
-        document.getElementById('analysisResults').classList.value = "mt-2 text-dark";
+        $('#analysisResults').addClass('mt-2 text-dark');
     }
     else {
-        document.getElementById('analysisResults').classList.value = "mt-2 text-white";
+        $('#analysisResults').addClass('mt-2 text-white');
     }
 
-    document.getElementById("contentContainer").hidden = false;
-    document.getElementById("contentContainer").classList.value = `rounded p-3 ${chartBackground}`;
-    document.getElementById("downloadBtn").classList.remove("disabled"); // Enable download button once chart is plotted
-    document.getElementById("analyzeBtn").classList.remove("disabled"); // Enable analyze button once chart is plotted
+    $('#contentContainer').show();
+    $('#contentContainer').removeClass();
+    $('#contentContainer').addClass(`rounded p-3 ${chartBackground}`);
+    $('#downloadBtn').removeClass('disabled');
+    $('#analyzeBtn').removeClass('disabled');
 }
 
 /**
@@ -257,10 +262,11 @@ function analyzeData() {
     editor.save().then((outputData) => {
         console.log('Article data: ', outputData)
         editor.render(outputData);
-      }).catch((error) => {
+    }).catch((error) => {
         console.log('Saving failed: ', error)
-      });
-    document.getElementById('analysisResults').hidden = false;
+    });
+
+    $('#analysisResults').show();
 }
 
 
